23b59f147ff2391b0b9a7f1d54cfdad2e0f4469c
Joerg Lauer <lauer.joerg@gmail.com>
2014-04-24 20:47:13 +0200

    Refactor code, preparation for touch events

--- montagsmaler.js
+++ montagsmaler.js
@@ -19,31 +19,61 @@ if (Meteor.isClient) {
     Meteor.subscribe("lines");
 
     var redraw = function () {
-            "use strict";
-            var chart, line;
-    
-            chart = d3.select(".drawport svg");
-    
-            line = chart.selectAll("line")
-                .data(Lines.find().fetch());
-                
-            line.exit().remove();
-            line.enter().append("line");
-    
-            line.attr("x1", function (d) {
-                    return d.beginX;
-                })
-                .attr("y1", function (d) {
-                    return d.beginY;
-                })
-                .attr("x2", function (d) {
-                    return d.endX;
-                })
-                .attr("y2", function (d) {
-                    return d.endY;
-                });
-    
-        };
+        "use strict";
+        var chart, line;
+
+        chart = d3.select(".drawport svg");
+
+        line = chart.selectAll("line")
+            .data(Lines.find().fetch());
+
+        line.exit().remove();
+        line.enter().append("line");
+
+        line.attr("x1", function (d) {
+            return d.beginX;
+        })
+            .attr("y1", function (d) {
+                return d.beginY;
+            })
+            .attr("x2", function (d) {
+                return d.endX;
+            })
+            .attr("y2", function (d) {
+                return d.endY;
+            });
+
+    };
+
+    var drawLine = function (begin, end) {
+        Lines.insert({
+            beginX: begin.x,
+            beginY: begin.y,
+            endX: end.x,
+            endY: end.y
+        });
+    }
+
+    var startSession = function (coords) {
+        Session.set("coords", coords);
+        Session.set("started", true);
+    }
+
+    var endSession = function (coords) {
+        if (Session.get("started")) {
+            drawLine(Session.get("coords"), coords);
+
+            Session.set("coords", null);
+            Session.set("started", false);
+        }
+    }
+
+    var continueSession = function (coords) {
+        if (Session.get("started")) {
+            drawLine(Session.get("coords"), coords);
+            Session.set("coords", coords);
+        }
+    }
 
     Deps.autorun(redraw);
 
@@ -52,53 +82,26 @@ if (Meteor.isClient) {
     };
 
     Template.hello.events({
-        'click input' : function() {
-            Meteor.call('clearArea',function(err, response) {});
+        'click input': function () {
+            Meteor.call('clearArea', function (err, response) {});
         }
     });
 
     Template.drawport.events({
+        //'touchmove '
         'mousemove .drawport svg': function (event) {
-            if (Session.get("started")) {
-                var coords, oldCoords;
-                oldCoords = Session.get("coords");
-
-                coords = coordsRelativeToElement(event.currentTarget, event);
-
-                Lines.insert({
-                    beginX: oldCoords.x,
-                    beginY: oldCoords.y,
-                    endX: coords.x,
-                    endY: coords.y,
-                });
-
-                Session.set("coords", coords);
-            }
+            continueSession(coordsRelativeToElement(event.currentTarget, event));
         },
+        //'touchstart
         'mousedown .drawport svg': function (event) {
-            Session.set("coords", coordsRelativeToElement(event.currentTarget, event));
-            Session.set("started", true);
+            startSession(coordsRelativeToElement(event.currentTarget, event));
         },
+        //'touchend '
         'mouseup .drawport svg': function (event) {
-            if (Session.get("started")) {
-                var coords, oldCoords;
-                oldCoords = Session.get("coords");
-
-                coords = coordsRelativeToElement(event.currentTarget, event);
-
-                Lines.insert({
-                    beginX: oldCoords.x,
-                    beginY: oldCoords.y,
-                    endX: coords.x,
-                    endY: coords.y,
-                });
-
-                Session.set("coords", null);
-                Session.set("started", false);
-            }
+            endSession(coordsRelativeToElement(event.currentTarget, event));
         }
     });
-    Template.drawport.rendered = function() {
+    Template.drawport.rendered = function () {
         var width, height, chart;
 
         width = 420;
@@ -107,7 +110,7 @@ if (Meteor.isClient) {
         chart = d3.select(".drawport svg")
             .attr("width", width)
             .attr("height", height);
-        
+
         redraw();
     }
 }
@@ -115,9 +118,9 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
     Meteor.startup(function () {
         Meteor.methods({
-            clearArea : function(){
+            clearArea: function () {
                 Lines.remove({});
             }
-        });        
+        });
     });
 }
